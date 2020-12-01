e8f8afcb377f31fcffb6f2dbc884973aecbcda8f
Rifki Computer <rifqiawali14@gmail.com>
2020-12-01 21:18:58 +0700

    Book Mapper Update
    Update

--- src/main/java/com/g2academy/gramedia/service/mapper/BookMapper.java
+++ src/main/java/com/g2academy/gramedia/service/mapper/BookMapper.java
@@ -1,8 +1,12 @@
 package com.g2academy.gramedia.service.mapper;
 
+import com.g2academy.gramedia.domain.Author;
 import com.g2academy.gramedia.domain.Book;
+import com.g2academy.gramedia.domain.Publisher;
 import com.g2academy.gramedia.service.Dto.BookDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -11,9 +15,25 @@ import java.util.List;
 public interface BookMapper {
     BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
 
+    @Mapping(target = "id", expression = "java(entity.getBookId())")
+    @Mapping(target = "authorId", expression = "java(entity.getAuthor().getAuthorId())")
+    @Mapping(target = "publisherId", expression = "java(entity.getPublisher().getPublisherId())")
+    @Mapping(source = "atuhor", target = "authorName", qualifiedByName = "getAuthorName")
+    @Mapping(source = "atuhor", target = "publisherName", qualifiedByName = "getPublisherName")
+
     BookDto toDto(Book entity);
     Book toEntities(BookDto dto);
 
     List<BookDto> toDtos(List<Book> Entities);
     List<Book> toEntities(List<BookDto> dtos);
+
+    @Named("getAuthorName")
+    default String authorEntityGetAuthorId(Author authorEntity){
+        return authorEntity.getName();
+    }
+
+    @Named("getPublisherName")
+    default String authorEntityGetAuthorId(Publisher publisherEntity){
+        return publisherEntity.getName();
+    }
 }
